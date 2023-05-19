e4519e9f508a0244b95570e179b80974b66b493d
yangfeng <yangfeng19980715@gmail.com>
2023-05-20 00:55:29 +0800

    chapter 19 finished

--- chapter19/chapter19_1/src/main.rs
+++ chapter19/chapter19_1/src/main.rs
@@ -1,3 +1,15 @@
+static mut COUNTER: u32 = 0;
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    add_to_count(3);
+
+    unsafe {
+        println!("COUNTER: {}", COUNTER);
+    }
 }
